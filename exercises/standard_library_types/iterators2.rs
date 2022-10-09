9725301d39f3b02c9cb83c7e2998c59f66dd6cfa
naty <aixushuai@sina.com>
2022-10-09 11:11:49 +0800

    first commit

---
+++ exercises/standard_library_types/iterators2.rs
@@ -0,0 +1,81 @@
+// iterators2.rs
+// In this exercise, you'll learn some of the unique advantages that iterators
+// can offer. Follow the steps to complete the exercise.
+// Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a hint.
+
+
+// Step 1.
+// Complete the `capitalize_first` function.
+// "hello" -> "Hello"
+pub fn capitalize_first(input: &str) -> String {
+    
+    let mut c = input.chars();
+    match c.next() {
+        None => String::new(),
+        Some(f) => f.to_uppercase().chain(c).collect(),
+        
+    }
+    
+}
+
+// Step 2.
+// Apply the `capitalize_first` function to a slice of string slices.
+// Return a vector of strings.
+// ["hello", "world"] -> ["Hello", "World"]
+pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
+    let mut res:Vec<String>  = vec![];
+
+    let v_iter=words.iter();
+    for s1 in v_iter{
+        let s2 =  capitalize_first(s1);
+        res.push(s2);
+    }
+    res
+    
+}
+    
+
+// Step 3.
+// Apply the `capitalize_first` function again to a slice of string slices.
+// Return a single string.
+// ["hello", " ", "world"] -> "Hello World"
+pub fn capitalize_words_string(words: &[&str]) -> String {
+    let mut res = String::new();
+
+    let v_iter=words.iter();
+    for s1 in v_iter{
+        let s2 =  capitalize_first(s1);
+        res += &s2;
+    }
+    res
+    
+
+    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_success() {
+        assert_eq!(capitalize_first("hello"), "Hello");
+    }
+
+    #[test]
+    fn test_empty() {
+        assert_eq!(capitalize_first(""), "");
+    }
+
+    #[test]
+    fn test_iterate_string_vec() {
+        let words = vec!["hello", "world"];
+        assert_eq!(capitalize_words_vector(&words), ["Hello", "World"]);
+    }
+
+    #[test]
+    fn test_iterate_into_string() {
+        let words = vec!["hello", " ", "world"];
+        assert_eq!(capitalize_words_string(&words), "Hello World");
+    }
+}
